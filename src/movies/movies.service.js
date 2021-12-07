7ab4098e0a1f387d929a6ef8b44d2d5506fc6e35
Robel Aredo <ichabodvancha@yahoo.com>
2021-12-07 11:38:48 -0600

    edit update review request and list movie reviews request to show all desired info

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -2,12 +2,12 @@ const knex = require("../db/connection");
 const mapProperties = require("../utils/map-properties");
 
 const mapCritic = mapProperties({
-  // critic_id: "critic.critic_id",
+  critic_id: "critic.critic_id",
   preferred_name: "critic.preferred_name",
   surname: "critic.surname",
   organization_name: "critic.organization_name",
-  // "c.created_at": "critic.created_at",
-  // "c.updated_at": "critic.updated_at",
+  "c.created_at": "critic.created_at",
+  "c.updated_at": "critic.updated_at",
 })
 
 function list () {
@@ -41,7 +41,7 @@ function showings (movie_id) {
 function reviews (movie_id) {
   return knex("reviews as r")
     .join("critics as c", "r.critic_id", "c.critic_id")
-    .select("*")
+    .select("*", "c.updated_at as c.updated_at", "c.created_at as c.created_at")
     .where({ movie_id })
     .then(reviews => reviews.map(mapCritic));
 }
