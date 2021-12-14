bfa42031efc3748696f683a9b3be5f0207de8e7a
Robel Aredo <ichabodvancha@yahoo.com>
2021-12-14 10:47:12 -0600

    the serial numbers reset when the db is reseeded for Postgres

--- src/db/seeds/00_drop_tables.js
+++ src/db/seeds/00_drop_tables.js
@@ -5,5 +5,9 @@ module.exports.seed = function (knex) {
     .then(() => knex("movies_theaters").del())
     .then(() => knex("critics").del())
     .then(() => knex("movies").del())
-    .then(() => knex("theaters").del());
-};
+    .then(() => knex("theaters").del())
+    .then(() => knex.raw("alter sequence critics_critic_id_seq restart with 1"))
+    .then(() => knex.raw("alter sequence movies_movie_id_seq restart with 1"))
+    .then(() => knex.raw("alter sequence reviews_review_id_seq restart with 1"))
+    .then(() => knex.raw("alter sequence theaters_theater_id_seq restart with 1"))
+  };
